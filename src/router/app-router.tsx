a456a9afd8329641f865816aec80826e48232b5b
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-02-26 19:27:37 +0100

    Add 404 not found and tests

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,16 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import { Home } from "../pages/Home/Home";
+import NotFound from "../pages/NotFound/NotFound";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <NotFound />,
 
     children: [
       {
         path: "",
         element: <Home />,
       },
+      {},
     ],
   },
 ]);
