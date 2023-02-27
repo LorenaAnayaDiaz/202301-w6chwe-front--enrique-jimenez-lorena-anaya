2fe72b69172e44b3ac28e39295e6a8d129e9044a
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-02-27 19:38:46 +0100

    add memory router in home test

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -3,15 +3,20 @@ import { Home } from "./Home";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../app/store";
+import { MemoryRouter } from "react-router-dom";
 
 test("When rendering home, the title should be displayed", () => {
   render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    </React.StrictMode>
+    <MemoryRouter>
+      <React.StrictMode>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      </React.StrictMode>
+    </MemoryRouter>
   );
+  const linkElement = screen.getByRole("link");
+  expect(linkElement).toBeInTheDocument();
   const title = screen.getByText("Check out the robots!");
   expect(title).toBeInTheDocument();
 });
