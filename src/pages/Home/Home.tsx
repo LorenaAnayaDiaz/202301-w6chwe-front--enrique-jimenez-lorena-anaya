5910f512a0edcd32a84184451b1e9d3f02d6b30c
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-02-27 19:28:41 +0100

    add form page and add style to pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import CardList from "../../features/cardlist/CardList";
 
 import "./Home.css";
@@ -6,8 +7,10 @@ export const Home = () => {
   return (
     <>
       <h1 className="home__title">Check out the robots!</h1>
-
       <CardList />
+      <Link className="text" data-testid="link" to={"/Form"}>
+        CREATE YOUR OWN ROBOTS!
+      </Link>{" "}
     </>
   );
 };
